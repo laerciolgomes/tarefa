2b6449c420a6bd24fa2c5d711753676cf546c0ab
web6600 <web6600@caelum55-01>
2017-01-18 21:39:38 -0200

    aula servlet

---
+++ fj21-agenda/src/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,26 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/oi")
+public class OiMundo extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira ServLet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Oi mundo Servlet, hahahah</h1>");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+}
